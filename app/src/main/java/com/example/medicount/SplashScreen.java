33209dec4158aa675fc57549a979a4168f0a1c78
nebrask <nebraskhan12@gmail.com>
2023-04-01 17:52:59 -0400

    Created splash screen for improved user interface

---
+++ app/src/main/java/com/example/medicount/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.medicount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler mhandler = new Handler();
+        mhandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+    }
+}
